b168e81a85a43b79dc1067594ca21ed1d01ccbd4
Rina-Mbugua <134137276+Rina-Mbugua@users.noreply.github.com>
2023-09-16 17:54:28 +0300

    changed border to shorthand

--- java.js
+++ java.js
@@ -6,10 +6,10 @@ function create () {
 
     for (var i = 0; i < noBoxes; i++) {
         var div = document.createElement("div");
-        div.style.border = "solid";
-        div.style.borderColor = "red";
+        div.style.border = " 0.5px solid grey";
         div.style.height = "30px";
         div.style.width = "30px";
+
         container.appendChild(div)
     }
 }
