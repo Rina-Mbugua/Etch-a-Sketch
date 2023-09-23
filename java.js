627f31143d1e7dd6a4d50e47024aa7d5fff38651
Rina-Mbugua <134137276+Rina-Mbugua@users.noreply.github.com>
2023-09-23 19:52:06 +0300

    add eventListener, boxSizeLabel funtion & gridSize

--- java.js
+++ java.js
@@ -2,15 +2,28 @@ const container = document.getElementById("div-container");
 const boxSizeRange = document.getElementById("boxSizeRange");
 const boxSizeLabel = document.getElementById("boxSizeLabel"); 
 
+//put and event listener to the boxSizeRange
+
+boxSizeRange.addEventListener("input", updateBoxSizeLabel);
+
+function updateBoxSizeLabel() {
+    const selectedSize = boxSizeRange.value;
+    boxSizeLabel.textContent = `Grid Size: $(selectedSize)x$(selectedSize)`
+}
+
+
 function create () {
     var form = document.getElementById("boxForm");
     noBoxes = form.elements['noBox'].value;
 
+    //calculate the size of boxes 
+    const gridSize = Math.floor(480/selectedSize);
+
     for (var i = 0; i < noBoxes; i++) {
         var div = document.createElement("div");
         div.style.border = " 0.5px solid grey";
-        div.style.height = "7.5px";
-        div.style.width = "7.5px";
+        div.style.height = `$(gridSize)`; //based on selectedSize
+        div.style.width = `$(gridSize)`; //based on selectedSize
         div.style.boxSizing = "border-box";
 
         container.appendChild(div)
